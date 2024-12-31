62db6498f227f2739be38d9398fdd7d8a393441a
xcrazibl3 <xcrazibl3@gmail.com>
2024-12-31 16:55:25 +0200

    Accordion component finished

--- src/App.js
+++ src/App.js
@@ -1,5 +1,37 @@
+import { useState } from "react";
+import { faqs } from "./faqs";
+
 function App() {
-  return <div>Hello React!</div>;
+  return (
+    <div>
+      <Accordion data={faqs} />
+    </div>
+  );
 }
 
+function Accordion({ data }) {
+  return (
+    <div className="accordion">
+      {data.map((item, i) => (
+        <AccordionItem item={item} number={i} key={i} />
+      ))}
+    </div>
+  );
+}
+
+function AccordionItem({ item, number }) {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <div
+      className={`item ${isOpen ? "open" : ""}`}
+      onClick={() => setIsOpen(!isOpen)}
+    >
+      <p className="number">{number < 10 ? `0${number + 1}` : number + 1}</p>
+      <p className="title">{item.title}</p>
+      <p className="icon">{isOpen ? "-" : "+"}</p>
+      {isOpen && <div className="content-box">{item.text}</div>}
+    </div>
+  );
+}
 export default App;
