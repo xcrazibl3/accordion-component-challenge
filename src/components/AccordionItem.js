e7c991eb26c0cb4caa5720a1f1f2dad504b2e50b
xcrazibl3 <xcrazibl3@gmail.com>
2024-12-31 18:10:00 +0200

    Fixed the bug with closing the component

--- src/components/AccordionItem.js
+++ src/components/AccordionItem.js
@@ -1,9 +1,13 @@
 export default function AccordionItem({ item, number, curOpen, onOpen }) {
+  function handleToggle(isOpen) {
+    if (isOpen) onOpen(null);
+    if (!isOpen) onOpen(number);
+  }
   const isOpen = curOpen === number ? true : false;
   return (
     <div
       className={`item ${isOpen ? "open" : ""}`}
-      onClick={() => onOpen(number)}
+      onClick={() => handleToggle(isOpen)}
     >
       <p className="number">{number < 10 ? `0${number + 1}` : number + 1}</p>
       <p className="title">{item.title}</p>
